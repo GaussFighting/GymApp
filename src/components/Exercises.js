96308cbca9b2a6c4f7d721fab5c5c03466eb2da5
GaussFighting <krzysztofkowerczyk@gmail.com>
2022-03-07 19:40:57 +0100

    preparing exercises component

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,5 +1,20 @@
-function Exercises() {
-  return <div>1234</div>;
-}
+import dummyExercises from "../data/dummyExercises.json";
+import React from "react";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
+
+const Exercises = () => {
+  const exercisesList = dummyExercises.map((exercise, idx) => (
+    <ListGroup key={exercise.id}>
+      <ListGroupItem>
+        {idx + 1} {exercise.exerciseEn} {exercise.bodyPart} {exercise.equipment}
+      </ListGroupItem>
+    </ListGroup>
+  ));
+  return (
+    <div>
+      <ul>{exercisesList}</ul>
+    </div>
+  );
+};
 
 export default Exercises;
