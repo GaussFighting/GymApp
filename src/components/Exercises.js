ac724d938c744ec97d8b7a0fd929c86f597270bb
GaussFighting <krzysztofkowerczyk@gmail.com>
2022-03-08 18:51:35 +0100

    Exercises button and css added

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,18 +1,22 @@
 import dummyExercises from "../data/dummyExercises.json";
 import React from "react";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
+import { Button } from "reactstrap";
 
 const Exercises = () => {
   const exercisesList = dummyExercises.map((exercise, idx) => (
     <ListGroup key={exercise.id}>
-      <ListGroupItem>
-        {idx + 1} {exercise.exerciseEn} {exercise.bodyPart} {exercise.equipment}
-      </ListGroupItem>
+      <Button outline>
+        <ListGroupItem className="text-align-exercise">
+          {idx + 1} {exercise.exerciseEn} {exercise.bodyPart}{" "}
+          {exercise.equipment}
+        </ListGroupItem>
+      </Button>
     </ListGroup>
   ));
   return (
-    <div>
-      <ul>{exercisesList}</ul>
+    <div className="margin-top">
+      <ul className="ul-exercise">{exercisesList}</ul>
     </div>
   );
 };
