4dd18f00f96ff1dca12ddd5f14537cff75b06e40
GaussFighting <krzysztofkowerczyk@gmail.com>
2022-04-03 18:39:46 +0200

    data migration to mongoDB

---
+++ server/routes/record.js
@@ -0,0 +1,73 @@
+const express = require("express");
+
+// recordRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /record.
+const recordRoutes = express.Router();
+
+// This will help us connect to the database
+const dbo = require("../db/conn");
+
+// This help convert the id from string to ObjectId for the _id.
+const ObjectId = require("mongodb").ObjectId;
+
+// This section will help you get a list of all the records.
+recordRoutes.route("/").get(function (req, res) {
+  let db_connect = dbo.getDb("GYMAPP");
+  db_connect
+    .collection("Exercises")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// This section will help you get a single record by id
+recordRoutes.route("/exercise/:id").get(function (req, res) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("Exercises").findOne(myquery, function (err, result) {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
+// This section will help you create a new record.
+recordRoutes.route("/exercise").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myobj = {
+    nameEn: req.body.exerciseName,
+    bodyPart: req.body.selectedBodyPart,
+    equipment: req.body.selectedEquipment,
+  };
+  db_connect.collection("Exercises").insertOne(myobj, function (err, res) {
+    if (err) throw err;
+    response.json(res);
+  });
+});
+
+// // This section will help you update a record by id.
+// recordRoutes.route("/update/:id").post(function (req, response) {
+//   let db_connect = dbo.getDb();
+//   let myquery = { _id: ObjectId( req.params.id )};
+//   let newvalues = {
+//     $set: {
+//       name: req.body.name,
+//       position: req.body.position,
+//       level: req.body.level,
+//   },
+// };
+
+// // This section will help you delete a record
+recordRoutes.route("/exercise/:id").delete((req, response) => {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("Exercises").deleteOne(myquery, function (err, obj) {
+    if (err) throw err;
+    console.log("1 document deleted");
+    response.json(obj);
+  });
+});
+
+module.exports = recordRoutes;
