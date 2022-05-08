6b12e27b53bef01431b31fafdd3345fdd853180d
GaussFighting <krzysztofkowerczyk@gmail.com>
2022-05-08 20:10:27 +0200

    Adding Template component

--- server/routes/record.js
+++ server/routes/record.js
@@ -47,7 +47,7 @@ recordRoutes.route("/exercise/:id").get(function (req, res) {
 });
 
 // This section will help you get a single record by id
-recordRoutes.route("/templates/:id").get(function (req, res) {
+recordRoutes.route("/template/:id").get(function (req, res) {
   let db_connect = dbo.getDb();
   let myquery = { _id: ObjectId(req.params.id) };
   db_connect.collection("Templates").findOne(myquery, function (err, result) {
