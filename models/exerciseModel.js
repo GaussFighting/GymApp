a2543e1c2b2b42f5cf689e6a96beb8092670301f
GaussFighting <krzysztofkowerczyk@gmail.com>
2022-09-06 20:44:09 +0200

    sketch

--- models/exerciseModel.js
+++ models/exerciseModel.js
@@ -4,21 +4,21 @@ const Schema = mongoose.Schema;
 
 const exerciseSchema = new Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  nameEn: {
-    type: String,
-    required: [true, "nameEn field is required"],
-    max: 200,
-  },
-  bodyPart: {
-    type: String,
-    required: [true, "bodyPart field is required"],
-    max: 200,
-  },
-  equipment: {
-    type: String,
-    required: [true, "equipment field is required"],
-    max: 200,
-  },
+  // nameEn: {
+  //   type: String,
+  //   required: [true, "nameEn field is required"],
+  //   max: 200,
+  // },
+  // bodyPart: {
+  //   type: String,
+  //   required: [true, "bodyPart field is required"],
+  //   max: 200,
+  // },
+  // equipment: {
+  //   type: String,
+  //   required: [true, "equipment field is required"],
+  //   max: 200,
+  // },
 });
 
 module.exports = mongoose.model("Exercises", exerciseSchema);
