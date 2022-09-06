6ee02e190fa61cb6fa13f2712b4cc9d6fa101bc7
GaussFighting <krzysztofkowerczyk@gmail.com>
2022-09-06 18:58:14 +0200

    sketch

---
+++ db/connectDb.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const connectDb = async (url) => {
+  //Here you can observe that url of mongo atlas will show once
+  //Check this in netlify functions panel after you deploy your page
+  //Observe it when you send form using url netlify function
+  console.log(url, "url to mongo atlas, connectDb");
+
+  await mongoose.connect(url);
+};
+
+module.exports = connectDb;
