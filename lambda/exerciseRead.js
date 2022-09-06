a86bffd0bd0acdebd00572e16c84a2b1867d50d2
GaussFighting <krzysztofkowerczyk@gmail.com>
2022-09-06 20:08:16 +0200

    sketch

--- lambda/exerciseRead.js
+++ lambda/exerciseRead.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
-// const connectDb = require("../db/connectDb");
+const connectDb = require("../db/connectDb");
 const Exercise = require("../models/exerciseModel");
 
+connectDb(process.env.REACT_APP_DB);
+//Make connection with mongoose to mongo atlas outside handler
+
 exports.handler = async (event, context) => {
+  console.log(mongoose.connection.readyState, "ready state exercise");
+  console.log("EVENT", event);
+  console.log("CONTEXT", context);
+  context.callbackWaitsForEmptyEventLoop = false;
   // const id = event.queryStringParameters.id;
   try {
-    const dbconn = await mongoose.connect(process.env.REACT_APP_DB);
-    console.log(dbconn);
-    //Make connection with mongoose to mongo atlas outside handler
-    console.log(mongoose.connection.readyState, "ready state exercise");
-    console.log("EVENT", event);
-    console.log("CONTEXT", context);
-    context.callbackWaitsForEmptyEventLoop = false;
     // const exercises = id;
     // ? await Exercise.find({ _id: id })
     console.log(Exercise);
