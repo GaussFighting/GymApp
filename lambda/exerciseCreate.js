2db245b8916d721edca693008fcde4cfe268f206
GaussFighting <krzysztofkowerczyk@gmail.com>
2022-09-08 16:54:05 +0200

    netlifycreate

--- lambda/exerciseCreate.js
+++ lambda/exerciseCreate.js
@@ -7,7 +7,7 @@ exports.handler = async (event, context) => {
   try {
     const data = JSON.parse(event.body);
     const exercise = {
-      _id: mongoose.Schema.Types.ObjectId(),
+      _id: mongoose.Types.ObjectId(),
       nameEn: data.nameEn,
       bodyPart: data.bodyPart,
       equipment: data.equipment,
