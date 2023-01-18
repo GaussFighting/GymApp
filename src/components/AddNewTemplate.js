a0694d0591e8db160b35252b4527de681f675f70
GaussFighting <krzysztofkowerczyk@gmail.com>
2023-01-18 17:18:14 +0100

    error fixing, workout without template fiexed, code cleaning

--- src/components/AddNewTemplate.js
+++ src/components/AddNewTemplate.js
@@ -69,6 +69,8 @@ function AddNewTemplate() {
     });
   }, [addedExercises]);
 
+  console.log(exercises);
+
   let exercisesForTemplate = addedExercises.map((exercise, idx) => (
     <ListGroupItem
       className="text-align-exercise text-uppercase"
