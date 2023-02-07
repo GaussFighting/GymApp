106e088eb39a2f6908cce088c8536d8dd8337c92
GaussFighting <krzysztof.kowerczyk@gmail.com>
2023-02-07 20:01:15 +0100

    navbar changes

--- src/components/Navigationbar.js
+++ src/components/Navigationbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Nav, NavItem } from "reactstrap";
+import { Navbar, Nav, NavItem } from "reactstrap";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -12,32 +12,34 @@ import {
 function Navigationbar() {
   return (
     <div>
-      <Nav justified pills>
-        <NavItem>
-          <NavLink className="nav-link" to="/">
-            Profile <br></br>
-            <FontAwesomeIcon icon={faUser} size="2x" />
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink className="nav-link" to="/exercises">
-            Exercises <br></br>
-            <FontAwesomeIcon icon={faDumbbell} size="2x" />
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink className="nav-link" to="/history">
-            History <br></br>
-            <FontAwesomeIcon icon={faMedal} size="2x" />
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink className="nav-link" to="/startworkout">
-            Start Workout <br></br>
-            <FontAwesomeIcon icon={faHeartPulse} size="2x" />
-          </NavLink>
-        </NavItem>
-      </Nav>
+      <Navbar color="primary">
+        <Nav justified="true" pills>
+          <NavItem>
+            <NavLink className="nav-link" to="/">
+              Profile <br></br>
+              <FontAwesomeIcon icon={faUser} size="2x" />
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to="/exercises">
+              Exercises <br></br>
+              <FontAwesomeIcon icon={faDumbbell} size="2x" />
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to="/history">
+              History <br></br>
+              <FontAwesomeIcon icon={faMedal} size="2x" />
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to="/startworkout">
+              Start Workout <br></br>
+              <FontAwesomeIcon icon={faHeartPulse} size="2x" />
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles/index.css";
+import "./styles/index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
